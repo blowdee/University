5b4418d5a0ba36b69910a962e2fa4e149ca907b7
Roman <blowdee1@gmail.com>
2017-12-12 18:44:12 +0200

    Add files via upload

---
+++ Theory of Algorithms/Models/Data.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ta5.Models
+{
+        public class TkModel
+        {
+            public int TkId { get; set; }
+        }
+}
